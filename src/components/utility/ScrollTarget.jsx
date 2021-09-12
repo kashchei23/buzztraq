50054b3e3254fce3542273a623d4163133824518
Daniel Brown <kashchei23@gmail.com>
2021-09-11 19:18:39 -0700

    Add ScrollToTop button

---
+++ src/components/utility/ScrollTarget.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ScrollTarget = () => (
+	<>
+		<div
+			className='scroll-target'
+			style={{
+				position: 'absolute',
+				top: '0',
+				height: '100vh',
+			}}
+		/>
+	</>
+);
+
+export default ScrollTarget;
